beb301277086346ac5679ae87f6ae98a816dd34c
Stefan Hudelmaier <hudelmaier@gmail.com>
2018-05-15 08:59:42 +0200

    Login with oauth2 basically works

--- src/main/java/name/hudelmaier/adminproxyportal/WebSecurityConfig.kt
+++ src/main/java/name/hudelmaier/adminproxyportal/WebSecurityConfig.kt
@@ -1,6 +1,7 @@
 package name.hudelmaier.adminproxyportal
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
@@ -10,9 +11,43 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder
 
 @Configuration
 @EnableWebSecurity
+@EnableOAuth2Sso
 class WebSecurityConfig(private val applicationSettings: ApplicationSettings) : WebSecurityConfigurerAdapter() {
 
 	override fun configure(http: HttpSecurity) {
+		//configureForLoginForm()
+		configureForOAuth2()
+	}
+
+	private fun configureForOAuth2() {
+
+		// @formatter:off
+		http
+				.csrf().disable()
+				.headers()
+					.frameOptions().disable()
+					.and()
+				.authorizeRequests()
+					.antMatchers("/__portal__/*.css").permitAll()
+					.antMatchers("/__portal__/login").permitAll()
+					.antMatchers("/__portal__/handleLogin").permitAll()
+					.antMatchers("/**").authenticated()
+					.and()
+				.oauth2Login()
+					.authorizationEndpoint()
+						.baseUri("/__portal__/oauth2/authorization")
+						.and()
+					.redirectionEndpoint()
+						.baseUri("/__portal__/login/oauth2/client/*")
+						.and()
+					.defaultSuccessUrl("/__portal__/index.html")
+
+		// @formatter:on
+
+	}
+
+	private fun configureForLoginForm() {
+
 		// @formatter:off
 		http
 				.csrf().disable()
@@ -32,7 +67,12 @@ class WebSecurityConfig(private val applicationSettings: ApplicationSettings) : 
 					.and()
 				.logout()
 					.logoutUrl("/__portal__/logout")
+					.and()
+				.oauth2Login()
+					.redirectionEndpoint()
+						.baseUri("/__portal__/login/oauth2/client/google")
 		// @formatter:on
+
 	}
 
 	@Autowired
